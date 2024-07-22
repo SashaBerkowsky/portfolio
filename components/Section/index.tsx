75c3593acbd2ef5458545f1cbd470d5f9ca587db
Sasha Berkowsky <s.nberkowsky@gmail.com>
2024-07-22 15:49:15 -0300

    new section colors

--- components/Section/index.tsx
+++ components/Section/index.tsx
@@ -2,7 +2,7 @@ export default function Section({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<div className='w-full text-foregroundHl-light dark:text-foregroundHl-dark bg-backgroundHl-light dark:bg-backgroundHl-dark p-6 rounded shadow-sm shadow-black-light dark:shadow-black-dark cursor-default'>
+		<div className='w-full text-brightWhite-light dark:text-black-light bg-backgroundHl-light dark:bg-brightCyan-dark p-6 rounded shadow-sm shadow-black-light dark:shadow-black-dark cursor-default'>
 			{children}
 		</div>
 	);
