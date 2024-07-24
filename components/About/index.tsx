e078367af495a513d87d3d447dab34e7413bbb46
Sasha Berkowsky <s.nberkowsky@gmail.com>
2024-07-23 23:05:26 -0300

    beta about me

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -16,7 +16,15 @@ export default function About() {
 			<div className='font-title text-xl text-center'>
 				{content.title[language]}
 			</div>
-			<div className='font-par'>test about</div>
+			<div className='font-par mt-2'>
+				Hi! My name is Sasha, {"I'm"} a proudly argentinan based certified
+				systems analyst and computer science student, dedicated full stack
+				developer with over 3 years of experience in software development.
+				Proactive, detail-oriented, and self-taught, with a proven ability to
+				work effectively in small, diverse teams. Currently seeking to advance
+				my technical skills and contribute to a collaborative, cross-functional
+				environment within a larger organization.
+			</div>
 		</Section>
 	);
 }
