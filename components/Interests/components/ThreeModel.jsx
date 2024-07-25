4fb5f01cdb7dbe595176c66e7d56861ee6d354a2
Sasha Berkowsky <s.nberkowsky@gmail.com>
2024-07-25 19:12:03 -0300

    small interests improvement

--- components/Interests/components/ThreeModel.jsx
+++ components/Interests/components/ThreeModel.jsx
@@ -4,8 +4,9 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { useLoader } from "@react-three/fiber";
 
-function ThreeModel({ isMobile = false }) {
-	const gltf = useLoader(GLTFLoader, "/interests/scene.glb");
+function KeyboardModel() {
+	const KEYBOARD_REF = "/interests/mechanical_keyboard.glb";
+	const gltf = useLoader(GLTFLoader, KEYBOARD_REF);
 	const groupRef = useRef();
 
 	useFrame((a) => {
@@ -22,12 +23,46 @@ function ThreeModel({ isMobile = false }) {
 	);
 }
 
-export default function ThreeModelCanvas() {
+function GameboyModel() {
+	const GAMEBOY_REF = "/interests/gameboy.glb";
+	const gltf = useLoader(GLTFLoader, GAMEBOY_REF);
+	const groupRef = useRef();
+
+	useFrame((a) => {
+		if (groupRef.current) {
+			// groupRef.current.rotation.x += 0.01;
+			groupRef.current.rotation.y += 0.01;
+		}
+	});
+
+	return (
+		<group
+			rotation={[0, -Math.PI / 4, Math.PI / 4]}
+			ref={groupRef}
+			position={[0, -3, 0]}
+		>
+			<primitive scale={1} object={gltf.scene} />
+		</group>
+	);
+}
+
+function getModel(id) {
+	switch (id) {
+		case 0:
+			return <KeyboardModel />;
+		case 1:
+			return <GameboyModel />;
+		default:
+			return <KeyboardModel />;
+	}
+}
+
+export default function ThreeModelCanvas({ modelNumber }) {
 	return (
 		<Canvas style={{ height: "400px" }}>
 			<ambientLight intensity={0.5} />
 			<pointLight position={[10, 10, 10]} />
-			<ThreeModel />
+			{getModel(modelNumber)}
 		</Canvas>
 	);
 }
